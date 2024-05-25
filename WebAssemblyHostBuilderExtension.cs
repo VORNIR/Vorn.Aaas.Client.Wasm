2c19483547cfeea6f7d0134c0ba532a439d6b422
Mobin <mobin.seven@live.com>
2024-05-25 09:20:53 +0330

    M: First working commit (Authentication Layout)

---
+++ WebAssemblyHostBuilderExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Vorn.Aaas.Client.Wasm;
+public static class WebAssemblyHostBuilderExtension
+{
+    public static WebAssemblyHostBuilder AddAaasClientWasm(this WebAssemblyHostBuilder builder)
+    {
+        builder.Services.AddAuthorizationCore();
+        builder.Services.AddCascadingAuthenticationState();
+        builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+        return builder;
+    }
+}
